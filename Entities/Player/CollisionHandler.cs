7dfc7be98cf6eb7b6d0a002e7feadbe0e8479fec
Noah <107080597+RichardBlevins@users.noreply.github.com>
2026-02-24 18:38:30 -0800

    made the oink projectile node and removed some stuff
    commented somestuff out that needs to be moved

--- Entities/Player/CollisionHandler.cs
+++ Entities/Player/CollisionHandler.cs
@@ -8,14 +8,14 @@ public partial class CollisionHandler : Node2D
     private List<AudioStreamPlayer2D> _oinks;
 	private RandomNumberGenerator _rng = new RandomNumberGenerator();
     private Player player;
-    private float eatTimer = 0f;
+    
     public override void _Ready()
     {
         base._Ready();
 
         player = GetParent<Player>();
 
-
+		//Move
 		_rng.Randomize();
 
 		_oinks = new List<AudioStreamPlayer2D>();
@@ -27,17 +27,23 @@ public partial class CollisionHandler : Node2D
                 _oinks.Add(audio);
             }
         }
+		//Move
     }
 
     public void PlayerInteractionsHandler()
 	{
+		//Move
 		bool spaceJustPressed = Input.IsActionJustPressed("ui_accept"); // creates a boolean var on the player spaceinput
 
 		if (spaceJustPressed) //checks if that input is true then ...
 		{
 			OinkSoundEffect();
-		}
 
+		}
+		// Move
+
+
+		//camera movment 
         Area2D hitbox = GetNode<Area2D>("Hitbox");
         foreach (Area2D area in hitbox.GetOverlappingAreas()) {
             if (area.IsInGroup("NRoom")) {
@@ -55,10 +61,9 @@ public partial class CollisionHandler : Node2D
     public override void _Process(double delta)
     {
         base._Process(delta);
-        		//EAT COOLDOWN
+		//Move
         PlayerInteractionsHandler();
     }
-
 
     private void OinkSoundEffect()
     {
@@ -68,7 +73,8 @@ public partial class CollisionHandler : Node2D
         int index = _rng.RandiRange(0, _oinks.Count - 1);
 
         var oink = _oinks[index];
-        oink.PitchScale = _rng.RandfRange(0.1f, 1f);
+        oink.PitchScale = _rng.RandfRange(1.6f, 1.8f);
         oink.Play();
     }
+	//Move
 }
